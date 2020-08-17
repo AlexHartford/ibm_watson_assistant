c5f4a96b6402cc414b9763a0bd1b1e281e846149
Alex Hartford <alex@oneblank.io>
2020-08-17 15:24:31 -0700

    Completed response models and updated example

--- lib/models.dart
+++ lib/models.dart
@@ -7,9 +7,9 @@ class IbmWatsonAssistantResponse {
   @JsonKey(name: 'output')
   final _Output output;
   @JsonKey(name: 'context')
-  final Map<String, dynamic> context;
+  final _Context context;
 
-  IbmWatsonAssistantResponse(this.output, this.context);
+  IbmWatsonAssistantResponse({this.output, this.context});
 
   factory IbmWatsonAssistantResponse.fromJson(Map<String, dynamic> json) =>
       _$IbmWatsonAssistantResponseFromJson(json);
@@ -25,13 +25,12 @@ class _Output {
   final List<_Intent> intents;
   @JsonKey(name: 'entities')
   final List<_Entity> entities;
-  // @JsonKey(name: 'actions')
-  // final List<_Actions> actions;
+  @JsonKey(name: 'actions')
+  final List<Map<String, dynamic>> actions;
   @JsonKey(name: 'generic')
   final List<_Generic> generic;
 
-  // _Output(this.intents, this.entities, this.actions, this.generic);
-  _Output(this.intents, this.entities, this.generic);
+  _Output({this.intents, this.entities, this.actions, this.generic});
 
   factory _Output.fromJson(Map<String, dynamic> json) => _$_OutputFromJson(json);
 
@@ -45,7 +44,7 @@ class _Intent {
   @JsonKey(name: 'confidence')
   final double confidence;
 
-  _Intent(this.intent, this.confidence);
+  _Intent({this.intent, this.confidence});
 
   factory _Intent.fromJson(Map<String, dynamic> json) => _$_IntentFromJson(json);
 
@@ -63,14 +62,12 @@ class _Entity {
   @JsonKey(name: 'confidence')
   final double confidence;
 
-  _Entity(this.entity, this.location, this.value, this.confidence);
+  _Entity({this.entity, this.location, this.value, this.confidence});
 
   factory _Entity.fromJson(Map<String, dynamic> json) => _$_EntityFromJson(json);
 
   Map<String, dynamic> toJson() => _$_EntityToJson(this);
 }
-
-// class _Actions {}
 
 @JsonSerializable()
 class _Generic {
@@ -79,9 +76,87 @@ class _Generic {
   @JsonKey(name: 'text')
   final String text;
 
-  _Generic(this.responseType, this.text);
+  _Generic({this.responseType, this.text});
 
   factory _Generic.fromJson(Map<String, dynamic> json) => _$_GenericFromJson(json);
 
   Map<String, dynamic> toJson() => _$_GenericToJson(this);
+}
+
+@JsonSerializable()
+class _Context {
+  @JsonKey(name: 'global')
+  final _Global global;
+  @JsonKey(name: 'skills')
+  final _Skills skills;
+
+  _Context({this.global, this.skills});
+
+  factory _Context.fromJson(Map<String, dynamic> json) => _$_ContextFromJson(json);
+
+  Map<String, dynamic> toJson() => _$_ContextToJson(this);
+}
+
+@JsonSerializable()
+class _Global {
+  @JsonKey(name: 'system')
+  final _System system;
+  @JsonKey(name: 'session_id')
+  final String sessionId;
+
+  _Global({this.system, this.sessionId});
+
+  factory _Global.fromJson(Map<String, dynamic> json) => _$_GlobalFromJson(json);
+
+  Map<String, dynamic> toJson() => _$_GlobalToJson(this);
+}
+
+@JsonSerializable()
+class _System {
+  @JsonKey(name: 'turn_count')
+  final int turnCount;
+
+  _System({this.turnCount});
+
+  factory _System.fromJson(Map<String, dynamic> json) => _$_SystemFromJson(json);
+
+  Map<String, dynamic> toJson() => _$_SystemToJson(this);
+}
+
+@JsonSerializable()
+class _Skills {
+  @JsonKey(name: 'main skill')
+  final _MainSkill mainSkill;
+
+  _Skills({this.mainSkill});
+
+  factory _Skills.fromJson(Map<String, dynamic> json) => _$_SkillsFromJson(json);
+
+  Map<String, dynamic> toJson() => _$_SkillsToJson(this);
+}
+
+@JsonSerializable()
+class _MainSkill {
+  @JsonKey(name: 'user_defined')
+  final Map<String, dynamic> userDefined;
+  @JsonKey(name: 'system')
+  final _SystemState systemState;
+
+  _MainSkill({this.userDefined, this.systemState});
+
+  factory _MainSkill.fromJson(Map<String, dynamic> json) => _$_MainSkillFromJson(json);
+
+  Map<String, dynamic> toJson() => _$_MainSkillToJson(this);
+}
+
+@JsonSerializable()
+class _SystemState {
+  @JsonKey(name: 'state')
+  final String state;
+
+  _SystemState({this.state});
+
+  factory _SystemState.fromJson(Map<String, dynamic> json) => _$_SystemStateFromJson(json);
+
+  Map<String, dynamic> toJson() => _$_SystemStateToJson(this);
 }
